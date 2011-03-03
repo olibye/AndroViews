4a929a49636e447d19adf4cfc191025f873b61d5
Steve Pomeroy <steve@staticfree.info>
2011-03-02 23:24:31 -0500

    Methods are all statically initialized.

--- andro-views/src/info/staticfree/android/utils/MotionEventWrapper.java
+++ andro-views/src/info/staticfree/android/utils/MotionEventWrapper.java
@@ -6,13 +6,37 @@ import android.os.Build;
 import android.view.MotionEvent;
 
 public class MotionEventWrapper {
+	public static final String TAG = MotionEventWrapper.class.getSimpleName();
+	private static Method
+		getX,
+		getY,
+		getPointerId,
+		findPointerIndex;
+	
+	private static final boolean IS_OLD = Build.VERSION.SDK_INT <= Build.VERSION_CODES.DONUT;
+	
+	static {
+		initCompatibility();
+	}
+	
+	private static void initCompatibility(){
+		if (!IS_OLD){
+			try {
+				getX = MotionEvent.class.getMethod("getX", int.class);
+				getY = MotionEvent.class.getMethod("getY", int.class);
+				getPointerId = MotionEvent.class.getMethod("getPointerId", int.class);
+				findPointerIndex = MotionEvent.class.getMethod("findPointerIndex", int.class);
+			} catch (Throwable e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
 	
     public static float getX(MotionEvent ev, int optPointerIndex){
-    	if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.DONUT){
+    	if (IS_OLD){
     		return ev.getX();
     	}else{
     		try {
-				Method getX = MotionEvent.class.getMethod("getX", int.class);
 				return ((Float) getX.invoke(ev, optPointerIndex)).floatValue();
 			} catch (Throwable e) {
 				throw new RuntimeException(e);
@@ -21,11 +45,10 @@ public class MotionEventWrapper {
     }
     
     public  static float getY(MotionEvent ev, int optPointerIndex){
-    	if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.DONUT){
+    	if (IS_OLD){
     		return ev.getY();
     	}else{
     		try {
-				Method getY = MotionEvent.class.getMethod("getY", int.class);
 				return ((Float) getY.invoke(ev, optPointerIndex)).floatValue();
 			} catch (Throwable e) {
 				throw new RuntimeException(e);
@@ -34,11 +57,10 @@ public class MotionEventWrapper {
     }
     
     public static int getPointerId(MotionEvent ev, int optPointerIndex){
-    	if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.DONUT){
+    	if (IS_OLD){
     		return 0;
     	}else{
     		try {
-				Method getPointerId = MotionEvent.class.getMethod("getPointerId", int.class);
 				return ((Integer) getPointerId.invoke(ev, optPointerIndex)).intValue();
 			} catch (Throwable e) {
 				throw new RuntimeException(e);
@@ -47,11 +69,10 @@ public class MotionEventWrapper {
     }
     
     public static int findPointerIndex(MotionEvent ev, int activePointerId){
-    	if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.DONUT){
+    	if (IS_OLD){
     		return 0;
     	}else{
     		try {
-				Method findPointerIndex = MotionEvent.class.getMethod("findPointerIndex", int.class);
 				return ((Integer) findPointerIndex.invoke(ev, activePointerId)).intValue();
 			} catch (Throwable e) {
 				throw new RuntimeException(e);
